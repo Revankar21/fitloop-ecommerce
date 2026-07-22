bba095daca18cbfb500488f091b7d9788204cb4f
Ved Revankar <102726112+Revankar21@users.noreply.github.com>
2026-07-22 11:31:24 +0530

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+# config.py
+
+PAYTM_MID = "YOUR_MID"
+PAYTM_MERCHANT_KEY = "YOUR_MERCHANT_KEY"
+PAYTM_WEBSITE = "WEBSTAGING"      # For testing
+PAYTM_CALLBACK_URL = "http://localhost:5000/paytm/callback"
+
+# Production
+# PAYTM_WEBSITE = "DEFAULT"
